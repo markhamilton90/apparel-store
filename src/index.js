88a76d01ed23b5c568e694476f92095386a42088
markhamilton90 <msh387@gmail.com>
2022-12-26 20:43:12 -0600

    disable routing for now

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
-import Router from './components/Router';
+// import Router from './components/Router';
+import App from './components/App';
 import './css/styles.css';
 import 'normalize.css';
 
-render(<Router />, document.getElementById('main'));
+render(<App />, document.getElementById('main'));
